3295fdb98cc18af14a42d9b039f0c3b94c960ca5
Jecidhz18 <jecidhz18@hotmail.com>
2023-11-21 15:59:32 -0600

    sin terminar

---
+++ RRHHCapucasCoffe/RRHHCapucasCoffe/Controllers/DeduccionesAplicadasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RRHHCapucasCoffe.Interfaces;
+
+namespace RRHHCapucasCoffe.Controllers
+{
+    public class DeduccionesAplicadasController : Controller
+    {
+        private readonly IRepositorioDeduccionAplicada repositorioDeduccionAplicada;
+
+        public DeduccionesAplicadasController(IRepositorioDeduccionAplicada repositorioDeduccionAplicada)
+        {
+            this.repositorioDeduccionAplicada = repositorioDeduccionAplicada;
+        }
+
+        [HttpGet]
+        public IActionResult DeduccionAplicada()
+        {
+            return View();
+        }
+    }
+}
---
+++ RRHHCapucasCoffe/RRHHCapucasCoffe/Services/RepositorioDeduccionAplicada.cs
@@ -0,0 +1,14 @@
+﻿using RRHHCapucasCoffe.Interfaces;
+
+namespace RRHHCapucasCoffe.Services
+{
+    public class RepositorioDeduccionAplicada : IRepositorioDeduccionAplicada
+    {
+        private readonly string connectionString;
+
+        public RepositorioDeduccionAplicada(IConfiguration configuration) 
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+    }
+}
